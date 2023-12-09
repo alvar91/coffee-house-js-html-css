01c237814411fe7a522395b8cc55608fb7cce32a
alvar91 <aleksey91scorp@bk.ru>
2023-12-09 17:04:46 +0300

    feat: abstract

---
+++ menu/scripts/views/abstract-view.js
@@ -0,0 +1,28 @@
+import { Render } from "../utils/render.js";
+
+export class AbstractView {
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error(`Can't instantiate Abstract, only concrete one.`);
+    }
+
+    this._element = null;
+    this._handler = {};
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = Render.createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  getTemplate() {
+    throw new Error(`Abstract method not implemented: getTemplate`);
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
